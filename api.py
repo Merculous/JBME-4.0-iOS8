2b60436e17ce5212191f9ee7c8467404a226bcb9
Merculous <24739823+Merculous@users.noreply.github.com>
2023-01-30 22:47:40 -0600

    Begin getting offsets automatically from a macos device remotely

--- api.py
+++ api.py
@@ -4,9 +4,13 @@ from remotezip import RemoteZip
 from urllib.request import urlopen
 
 
+def getJSONData(url: str) -> dict:
+    return json.loads(urlopen(url).read())
+
+
 def getVersionsForDevice(device: str) -> dict:
     url = f'https://api.ipsw.me/v4/device/{device}?type=ipsw'
-    return json.loads(urlopen(url).read())
+    return getJSONData(url)
 
 
 def getVersionURL(version: str, data: dict) -> str:
@@ -15,9 +19,25 @@ def getVersionURL(version: str, data: dict) -> str:
             return firmware['url']
 
 
-def downloadKernelFromURL(url: str):
+def iOSToBuildid(version: str, data: dict) -> str:
+    for firmware in data['firmwares']:
+        if firmware['version'] == version:
+            return firmware['buildid']
+
+
+def downloadKernelFromURL(url: str) -> None:
     with RemoteZip(url) as f:
         for path in f.filelist:
             if 'kernelcache' in path.filename:
                 with open(path.filename, 'wb') as k:
                     k.write(f.read(path.filename))
+
+
+def getKeysForVersion(device: str, version: str) -> dict:
+    data = getVersionsForDevice(device)
+    buildid = iOSToBuildid(version, data)
+    url = f'https://api.ipsw.me/v4/keys/ipsw/{device}/{buildid}'
+    keys = getJSONData(url)
+    for key in keys['keys']:
+        if key['image'] == 'Kernelcache':
+            return key
--- remote_offsetfinder.py
+++ remote_offsetfinder.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
-import paramiko
 import sys
 
-from api import getVersionsForDevice, getVersionURL, downloadKernelFromURL
+import paramiko
+from pathlib import Path
+
+from api import downloadKernelFromURL, getVersionsForDevice, getVersionURL, getKeysForVersion
+
+
+def getDecryptionCMD(device: str, version: str) -> str:
+    data = getKeysForVersion(device, version)
+    cmd = (
+        '/usr/local/bin/xpwntool',
+        'OF32/kernelcache.encrypted',
+        'OF32/kernelcache.decrypted',
+        f'-iv {data["iv"]}',
+        f'-k {data["key"]}'
+    )
+    return ' '.join(cmd)
 
 
 def getKernel(device: str, version: str) -> None:
@@ -12,11 +26,41 @@ def getKernel(device: str, version: str) -> None:
     downloadKernelFromURL(url)
 
 
+def findKernel() -> str:
+    for path in Path().glob('*'):
+        if 'kernelcache' in path.name:
+            return path
+
+
+def getOF32CMD() -> str:
+    cmd = (
+        'OF32/OF32',
+        'OF32/kernelcache.decrypted'
+    )
+    return ' '.join(cmd)
+
+
+def getOffsets(address: str, user: str, password: str, device: str, version: str) -> list:
+    getKernel(device, version)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(address, username=user, password=password)
+    sftp = ssh.open_sftp()
+    sftp.put(findKernel(), 'OF32/kernelcache.encrypted')
+    ssh.exec_command(getDecryptionCMD(device, version))
+    (cmd_in, cmd_out, cmd_err) = ssh.exec_command(getOF32CMD())
+    offsets = [o for o in cmd_out]
+    sftp.close()
+    ssh.close()
+    return offsets
+
+
 def main(args: list) -> None:
-    if len(args) == 3:
-        getKernel(args[1], args[2])
+    if len(args) == 6:
+        offsets = getOffsets(args[1], args[2], args[3], args[4], args[5])
+        print(offsets)
     else:
-        print('Usage: <device> <iOS>')
+        print('Usage: <address> <user> <password> <device> <ios>')
 
 
 if __name__ == '__main__':
